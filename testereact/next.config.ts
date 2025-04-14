bbcae8e6ac37b9e1e47dc68ae22e482b2cb33ca5
Igor Martins <iguinhoigor4@gmail.com>
2025-04-14 15:41:23 -0300

    feat: atualizar configuração do Next.js para exportação estática

--- testereact/next.config.ts
+++ testereact/next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export'
-};
-
-export default nextConfig;
+module.exports = {
+  // Nova configuração para exportação estática
+  output: 'export',  // Adicione isso para exportar como uma aplicação estática
+  trailingSlash: true,  // Garante que todas as URLs terminem com uma barra (opcional, mas recomendado para GitHub Pages)
+}
